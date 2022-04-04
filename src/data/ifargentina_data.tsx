65adcf2d4c3e89ac42dcc783f7dc87e2fdff859e
unknown <fiorinosanti@gmail.com>
2022-04-04 00:27:03 -0300

    better english

--- src/data/ifargentina_data.tsx
+++ src/data/ifargentina_data.tsx
@@ -1,29 +1,29 @@
 export const paragraphs = [
   <div>
     <p>
-      Argentina has a terrible inflation problem. One of the highest inflation
+      Argentina has a terrible inflation problem– one of the highest inflation
       rates in the world. This website lets you pick a country and then creates
       a simulated store where the prices are the ones Argentina would have if
       its inflation rates in the last 20 years were the same as that country.
-      This project is not politically biased in any way, it shows real world
-      data, shuch as a regular graph. This is my first project that got news
+      This project is not politically biased in any way– it shows real world
+      data, such as a regular graph. This is my first project that got news
       coverage, both on TV and newspapers.
     </p>
   </div>,
   <div>
-    Once you get the simulated store with the fake prices, under each product
-    there is a button which lets you see what's the real price of the product,
-    and it's never good news to see it. To get the “fake price” I did some
-    research on the prices of some products in Argentina in 2002. Then I looked
-    up some other countries’ cumulative inflation in the past 20 years, and if
-    you add that percentage to the 2002 price, you get how much they would cost
-    today with their inflation. Finally I got a list with the current price of
-    all the products, and that’s all of the information I needed for my website.
+    Once you get to the simulated store with the fake prices, under each product
+    there is a button which lets you see what the real price of the product is,
+    and it's never good news. To get the “fake price”, I did some research on
+    the prices of some products in Argentina in 2002. Then I looked up some
+    other countries’ cumulative inflations in the past 20 years, and if you add
+    that percentage to the 2002 price, you get how much they would cost today
+    with their inflation. Finally, I got a list with the current price of all
+    the products, and that’s all of the information I needed for the website.
   </div>,
   <div>
     The day after making the website public, two news reporters contacted me
-    asking for permission to show the website on TV, and asking about the
-    creation process of it and my sources. That same day it got featured on two
-    of the most popular Argentinian TV channels, and got three articles written.
+    asking for permission to show it on TV, and asking about how I created it
+    and which sources I used. That same day, it got featured on two of the most
+    popular Argentinian TV channels, and three articles were written about it.
   </div>,
 ];
--- src/data/onlineport_data.tsx
+++ src/data/onlineport_data.tsx
@@ -1,13 +1,14 @@
 export const paragraphs = [
   <div>
     <p>
-      Around two years ago I built my first personal portfolio. This year
-      looking at it I thought "it looks good, but it doesn't represent me very
-      well" which is the main goal. After some research I decided to build this
-      website inspired on Mondrian's work, and mixing it with some 80's graphic
-      elements. I think this colorful design is more alive, and it fits my
-      style. If you're a developer you know it's also a harder style to achieve,
-      so it really shows what I'm capable of, the other main goal.
+      Around two years ago I built my first personal portfolio. Looking back at
+      it this year, I thought "it looks good, but it doesn't represent me very
+      well", which is the main goal. After some research I decided to build this
+      website inspired by Mondrian's work, and mix it with some 80's graphic
+      elements. I think this colorful design is more alive, and fits my style
+      better. If you're a developer, you know it's also a harder style to
+      achieve, so it really shows what I'm capable of, which is the other main
+      goal.
     </p>
   </div>,
   <div>
@@ -15,7 +16,7 @@ export const paragraphs = [
       I try to get as close as possible to the "Designer" concept by using
       theory to solve problems and find solutions that are as functional as
       possible, but also using creativity to come up with new things, and keep
-      everything neat and elegant. ¿What does that have to do with this website?
+      everything neat and elegant. What does that have to do with this website?
       Designers usually show their work by making a portfolio, and that's
       essentially what this website is.
     </p>
@@ -24,13 +25,12 @@ export const paragraphs = [
     <p>
       I'm constantly starting new projects, so I wanted to make a place where I
       can exhibit all of them and my career. I learn new things from each
-      project, and not only new technologies and computer science stuff. Each
-      project also gives me knowledge of the topic its focused on, for example
-      my inflation project taught me a lot about economy. That's what pushes me
-      forward to make more projects, it's a win in every aspect I can look at. I
-      thought it would also work as some kind of interactive CV, which I can
-      show when I apply to a job, so I don't just say "I can do this or that",
-      but I show this instead.
+      project, and not only about new technologies and computer science. Each
+      project also gives me knowledge of the topic it's focused on– for example,
+      my inflation project taught me a lot about economics. That's what pushes
+      me forward to make more projects– in whichever aspect I look at it, it’s a
+      win for me. I thought it would also work as an interactive CV, which I can
+      use when applying to jobs.
     </p>
   </div>,
 ];
